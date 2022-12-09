a27a2be2794ac229da04ca6c45b1ca013c2566ee
= <=>
2022-12-09 15:14:26 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
-
+int n = 1;
+int nodd = 0, neven = 0;
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+     cout << "Enter an integer: ";
+     cin >> n;
+     while (n != 0)
+     {
+        if (n%2 == 0)
+        {
+            neven++;
+        }else{
+            nodd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> n;
+     }
+     
+    cout << "#Even numbers = "<< neven;
+    cout << "\n#Odd numbers = "<< nodd;
+    
     return 0;
 }
